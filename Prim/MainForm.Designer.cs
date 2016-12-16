3bae549af403319692218e6850f1e17cfb006405
IdiotMonsters <IdiotMonsters@IDIOTMONSTERS>
2016-12-16 21:13:03 +0700

    Add project files.

---
+++ Prim/MainForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Prim
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previousToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supportToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(541, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(478, 481);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(592, 625);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.supportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1031, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nextToolStripMenuItem,
+            this.previousToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.nextToolStripMenuItem.Text = "Next";
+            this.nextToolStripMenuItem.Click += new System.EventHandler(this.nextToolStripMenuItem_Click);
+            // 
+            // previousToolStripMenuItem
+            // 
+            this.previousToolStripMenuItem.Name = "previousToolStripMenuItem";
+            this.previousToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.previousToolStripMenuItem.Text = "Previous";
+            this.previousToolStripMenuItem.Click += new System.EventHandler(this.previousToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.resetToolStripMenuItem.Text = "Reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // supportToolStripMenuItem
+            // 
+            this.supportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supportToolStripMenuItem1});
+            this.supportToolStripMenuItem.Name = "supportToolStripMenuItem";
+            this.supportToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.supportToolStripMenuItem.Text = "Help";
+            // 
+            // supportToolStripMenuItem1
+            // 
+            this.supportToolStripMenuItem1.Name = "supportToolStripMenuItem1";
+            this.supportToolStripMenuItem1.Size = new System.Drawing.Size(116, 22);
+            this.supportToolStripMenuItem1.Text = "Support";
+            this.supportToolStripMenuItem1.Click += new System.EventHandler(this.supportToolStripMenuItem1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(933, 50);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "                                           IMPLEMENTASI ALGORITMA PRIMS GRAF\r\nDAL" +
+    "AM PEMBUATAN JALUR SISTEM TIANG LISTRIK DENGAN LINTASAN TERPENDEK DI ITS";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(673, 625);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "&Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 625);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "&MAP ITS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 481);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(912, 625);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "&Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 683);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(800, 726);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Prim\'s Minimum Spanning Tree by Group 10";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supportToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previousToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Prim/MainForm.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Prim
+{
+    public partial class MainForm : Form
+    {
+        private const int PAINTED = 0, UNPAINTED = 1;
+        private int index, n, x, y, x1, x2, y1, y2;
+        private int[] u, v;
+        private Color[] color;
+        private List<Node> graph;
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void supportToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Support by :" + Environment.NewLine + "MOHAMMAD FIKRI          5115100049" + Environment.NewLine + "GLENN LUCAS                    5115100148" + Environment.NewLine + "WAHYU PUJIONO              5115100151" + Environment.NewLine + "UNGGUL WIDODO W         5115100706");
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private PriorityQueue queue;
+
+        private void petaITSToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Size = new System.Drawing.Size(815, 490);
+            form.Show();
+
+            PictureBox button3 = new PictureBox();
+            button3.Image = Properties.Resources.MAP_ITS;
+            button3.Size = new System.Drawing.Size(815, 490);
+
+            form.Controls.Add(button3);
+        }
+
+        private void nextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index++;
+
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+
+        private void previousToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index--;
+
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            index = -2;
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            CreateGraph();
+            index = -1;
+            panel1.Paint += new PaintEventHandler(panel1_Paint);
+        }
+
+        private void CreateGraph()
+        {
+            graph = new List<Node>();
+            Node a = new Node(0, 0);
+            Node b = new Node(1, Node.INFINITY);
+            Node c = new Node(2, Node.INFINITY);
+            Node d = new Node(3, Node.INFINITY);
+            Node e = new Node(4, Node.INFINITY);
+            Node f = new Node(5, Node.INFINITY);
+            Node g = new Node(6, Node.INFINITY);
+
+            List<Node> adjA = a.Adjacency;
+            List<Node> adjB = b.Adjacency;
+            List<Node> adjC = c.Adjacency;
+            List<Node> adjD = d.Adjacency;
+            List<Node> adjE = e.Adjacency;
+            List<Node> adjF = f.Adjacency;
+            List<Node> adjG = g.Adjacency;
+
+            List<int> weightA = a.Weight;
+            List<int> weightB = b.Weight;
+            List<int> weightC = c.Weight;
+            List<int> weightD = d.Weight;
+            List<int> weightE = e.Weight;
+            List<int> weightF = f.Weight;
+            List<int> weightG = g.Weight;  
+            
+            adjA.Add(b);
+            adjA.Add(d);
+            adjA.Add(e);
+            adjA.Add(f);
+            adjA.Add(g);
+            weightA.Add(5);
+            weightA.Add(25);
+            weightA.Add(10);
+            weightA.Add(20);
+            weightA.Add(15);
+            adjB.Add(c);
+            adjB.Add(d);
+            adjB.Add(a);
+            weightB.Add(22);
+            weightB.Add(13);
+            weightB.Add(5);
+            adjC.Add(d);
+            adjC.Add(b);
+            weightC.Add(30);
+            weightC.Add(22);
+            adjD.Add(e);
+            adjD.Add(a);
+            adjD.Add(b);
+            weightD.Add(17);
+            weightD.Add(25);
+            weightD.Add(13);
+            adjE.Add(f);
+            adjE.Add(a);
+            adjE.Add(d);
+            weightE.Add(7);
+            weightE.Add(10);
+            weightE.Add(17);
+            adjF.Add(g);
+            adjF.Add(a);
+            adjF.Add(e);
+            weightF.Add(3);
+            weightF.Add(20);
+            weightF.Add(7);
+            adjG.Add(a);
+            adjG.Add(f);
+            weightG.Add(15);
+            weightG.Add(3);
+
+            graph.Add(a);
+            graph.Add(b);
+            graph.Add(c);
+            graph.Add(d);
+            graph.Add(e);
+            graph.Add(f);
+            graph.Add(g);
+            n = graph.Count;
+            color = new Color[n];
+
+            for (int m = 0; m < n; m++)
+
+                color[m] = Color.Red;
+
+            u = new int[n];
+            v = new int[n];
+
+            MST();
+        }
+
+
+
+        void MST()
+        {
+            bool[] output = new bool[n];
+            int[] pi = new int[n];
+            List<Node> copy = new List<Node>();
+
+            for (int i = 0; i < graph.Count; i++)
+                copy.Add(graph[i]);
+
+            queue = new PriorityQueue(copy);
+            queue.buildHeap();
+
+            for (int i = 0; i < queue.NodeList.Count; i++)
+            {
+                Node node = queue.extractMin();
+
+                output[node.Id] = true;
+                for (int j = 0; j < node.Adjacency.Count; j++)
+                {
+                    Node next = node.Adjacency[j];
+                    int weight = node.Weight[j];
+
+                    if (!output[next.Id] && weight <= next.Key)
+                    {
+                        pi[next.Id] = node.Id;
+                        node.Adjacency[j].Key = weight;
+                    }
+                }
+
+            }
+
+            pi[0] = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                u[i] = i;
+                v[i] = pi[i];
+            }
+
+            // reorder the edges in the minimum spanning tree
+            
+            for (int i = 0; i <= n - 1; i++)
+          {
+                for (int j = i + 1; j < n; j++)
+                {
+                    Node nodeI = graph[u[i]];
+                    Node nodeJ = graph[u[j]];
+
+                    if (v[i] >= v[j] && nodeI.Key > nodeJ.Key)
+                    {
+                        int t = u[i];
+
+                        u[i] = u[j];
+                        u[j] = t;
+                        t = v[i];
+                        v[i] = v[j];
+                        v[j] = t;
+                    }
+                }
+            }
+        }
+
+        private void calculateXY(int id)
+        {
+            int Width = panel1.Width;
+            int Height = panel1.Height;
+
+            x = Width / 2 + (int)(Width * Math.Cos(2 * id * Math.PI / n) / 4.0);
+            y = Height / 2 + (int)(Width * Math.Sin(2 * id * Math.PI / n) / 4.0);
+        }
+
+        private void draw(Graphics g)
+        {
+
+            if (index != -1)
+            {
+                int Width = panel1.Width;
+                int Height = panel1.Height;
+                Font font = new Font("Courier New", 12f, FontStyle.Bold);
+                List<Node> nodeList = queue.NodeList;
+                Pen pen = new Pen(Color.Black);
+                SolidBrush textBrush = new SolidBrush(Color.White);
+                //SolidBrush weightBrush = new SolidBrush(Color.Black);
+
+                for (int i = 0; i <= index; i++)
+                {
+                    if (v[i] != -1)
+                    {
+
+                        calculateXY(u[i]);
+                        x1 = x + (Width / 2) / n / 2;
+                        y1 = y + (Width / 2) / n / 2;
+                        calculateXY(v[i]);
+                        x2 = x + (Width / 2) / n / 2;
+                        y2 = y + (Width / 2) / n / 2;
+                        g.DrawLine(pen, x1, y1, x2, y2);                        
+                    }
+
+
+                    SolidBrush brush = new SolidBrush(color[u[index]]);
+
+                    char[] c = new char[1];
+
+                    c[0] = (char)('a' + u[i]);
+
+                    string str = new string(c);
+
+                    calculateXY(u[i]);
+                    g.FillEllipse(brush, x, y, (Width / 2) / n, (Width / 2) / n);
+                    g.DrawString(str, font,
+                        textBrush, (float)(x + (Width / 2) / n / 2) - 12f,
+                        (float)(y + (Width / 2) / n / 2) - 12f);
+
+
+                    if (v[i] != -1)
+                    {
+                        c = new char[1];
+                        c[0] = (char)('a' + v[i]);
+                        str = new string(c);
+                        calculateXY(v[i]);
+                        g.FillEllipse(brush, x, y, (Width / 2) / n, (Width / 2) / n);
+                        g.DrawString(str, font,
+                            textBrush, (float)(x + (Width / 2) / n / 2) - 12f,
+                            (float)(y + (Width / 2) / n / 2) - 12f);
+                    }
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs pea)
+        {
+            draw(pea.Graphics);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            index++;
+
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            index--;
+
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            index = -2;
+            if (index < n)
+                panel1.Invalidate();
+
+            else
+                index = -1;
+        }
+    }
+}
